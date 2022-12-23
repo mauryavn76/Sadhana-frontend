21b24483c7988456242f1185d93926fad2be7404
Ninad Muranjan <hrxprog69@gmail.com>
2022-12-23 13:09:05 +0530

    cewds

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -6,36 +6,33 @@ import { fontSize, margin, padding } from '@mui/system';
 import { Margin } from '@mui/icons-material';
 
 const Footer = () => {
-
-  // const [show, setShow] = useState(false);
-
   return (
     <footer className=" lg:text-left bg-dropdown-bg text-white">
-        <>
-      <div className="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300">
-        <div className="mr-12 hidden lg:block">
-          <span>Get connected with us on social networks:</span>
+      <>
+        <div className="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300">
+          <div className="mr-12 hidden lg:block">
+            <span>Get connected with us on social networks:</span>
+          </div>
+          <div className="flex justify-center text-white">
+            <a href="#!" className="mr-6">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#!" className="mr-6">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#!" className="mr-6">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+            <a href="#!">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </div>
         </div>
-        <div className="flex justify-center text-white">
-          <a href="#!" className="mr-6">
-            <i className="fa-brands fa-facebook-f"></i>
-          </a>
-          <a href="#!" className="mr-6">
-            <i className="fa-brands fa-instagram"></i>
-          </a>
-          <a href="#!" className="mr-6">
-            <i className="fa-brands fa-linkedin-in"></i>
-          </a>
-          <a href="#!">
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-        </div>
-      </div>
-      <div className="mx-6 py-10 text-center md:text-left">
-        <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="">
-            <h6
-              className="
+        <div className="mx-6 py-10 text-center md:text-left">
+          <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div className="">
+              <h6
+                className="
     uppercase
     font-semibold
     mb-4
@@ -44,89 +41,89 @@ const Footer = () => {
     justify-center
     md:justify-start
   "
-            >
-              Sadhana
-            </h6>
-            <div>
-              <h6 className="mb-4">
-                <a href="/contact-us">
-                  <AddIcCallIcon sx={{ m: 1 }} />
-                  Contact Us
-                </a>
+              >
+                Sadhana
               </h6>
+              <div>
+                <h6 className="mb-4">
+                  <a href="/contact-us">
+                    <AddIcCallIcon sx={{ m: 1 }} />
+                    Contact Us
+                  </a>
+                </h6>
+              </div>
+              <div>
+                <h6 className="mb-4">
+                  <a href="/about-us">
+                    <InfoIcon sx={{ m: 1 }} />
+                    About Us
+                  </a>
+                </h6>
+              </div>
             </div>
-            <div>
-              <h6 className="mb-4">
-                <a href="/about-us">
-                  <InfoIcon sx={{ m: 1 }} />
-                  About Us
-                </a>
+            <div className="text-white">
+              <h6 className="uppercase font-semibold mb-4 flex justify-center">
+                Products
               </h6>
+              <div className="text-gray-400">
+                <p className="mb-4">
+                  <a href="#!">Angular</a>
+                </p>
+                <p className="mb-4">
+                  <a href="#!">React</a>
+                </p>
+                <p className="mb-4">
+                  <a href="#!">Vue</a>
+                </p>
+                <p>
+                  <a href="#!">Laravel</a>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="text-white">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center">
-              Products
-            </h6>
-            <div className="text-gray-400">
-              <p className="mb-4">
-                <a href="#!">Angular</a>
-              </p>
-              <p className="mb-4">
-                <a href="#!">React</a>
-              </p>
-              <p className="mb-4">
-                <a href="#!">Vue</a>
-              </p>
-              <p>
-                <a href="#!">Laravel</a>
-              </p>
+            <div className="">
+              <h6 className="uppercase font-semibold mb-4 flex justify-center">
+                Info
+              </h6>
+              <div className="text-gray-400">
+                <p className="mb-4">
+                  <a href="#!">Pricing</a>
+                </p>
+                <p className="mb-4">
+                  <a href="#!">Settings</a>
+                </p>
+                <p className="mb-4">
+                  <a href="#!">Orders</a>
+                </p>
+                <p>
+                  <a href="#!">Help</a>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center">
-              Info
-            </h6>
-            <div className="text-gray-400">
-              <p className="mb-4">
-                <a href="#!">Pricing</a>
-              </p>
-              <p className="mb-4">
-                <a href="#!">Settings</a>
-              </p>
-              <p className="mb-4">
-                <a href="#!">Orders</a>
-              </p>
-              <p>
-                <a href="#!">Help</a>
-              </p>
-            </div>
-          </div>
-          <div className="">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
-              Contact
-            </h6>
-            <div className="text-gray-400">
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <i className="fa-solid fa-house-chimney "></i>
-                <span className="ml-4">New York, NY 10012, US</span>
-              </p>
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <i className="fa-solid fa-envelope"></i>
-                <span className="ml-4"> info@example.com </span>
-              </p>
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <i className="fa-solid fa-phone-flip"></i>
-                <span className="ml-4"> + 01 234 567 88 </span>
-              </p>
-              <p className="flex items-center justify-center md:justify-start">
-                <i className="fa-solid fa-print"></i>
-                <span className="ml-4"> + 01 234 567 89 </span>
-              </p>
+            <div className="">
+              <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
+                Contact
+              </h6>
+              <div className="text-gray-400">
+                <p className="flex items-center justify-center md:justify-start mb-4">
+                  <i className="fa-solid fa-house-chimney "></i>
+                  <span className="ml-4">New York, NY 10012, US</span>
+                </p>
+                <p className="flex items-center justify-center md:justify-start mb-4">
+                  <i className="fa-solid fa-envelope"></i>
+                  <span className="ml-4"> info@example.com </span>
+                </p>
+                <p className="flex items-center justify-center md:justify-start mb-4">
+                  <i className="fa-solid fa-phone-flip"></i>
+                  <span className="ml-4"> + 01 234 567 88 </span>
+                </p>
+                <p className="flex items-center justify-center md:justify-start">
+                  <i className="fa-solid fa-print"></i>
+                  <span className="ml-4"> + 01 234 567 89 </span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </>
     </footer>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,64 @@
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
-import { useRouter } from "next/router";
-import Sidebar from "../components/admin/admin-sidebar";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { CookiesProvider } from "react-cookie";
-import { useEffect, useState, useContext } from "react";
-import { AuthContext, AuthProvider } from "../utils/authContext";
-import NextNProgress from "nextjs-progressbar";
-import jwtDecode from "jwt-decode";
-import axios from "axios";
-import BASE_URL from "../utils/base-url";
-import AdminLayout from "../components/multiusable/admin-layout";
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import Sidebar from '../components/admin/admin-sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CookiesProvider } from 'react-cookie';
+import { useEffect, useState, useContext } from 'react';
+import { AuthContext, AuthProvider } from '../utils/authContext';
+import NextNProgress from 'nextjs-progressbar';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import BASE_URL from '../utils/base-url';
+import AdminLayout from '../components/multiusable/admin-layout';
 // import Login from './user/login';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [token, setToken] = useState("");
-  
+  const [token, setToken] = useState('');
+
   // console.log(router.pathname.split('/'));
   const getToken = () => {
-    if (typeof window !== "undefined") {
-      let token = localStorage.getItem("access-token");
+    if (typeof window !== 'undefined') {
+      let token = localStorage.getItem('access-token');
       return token;
     }
   };
 
   const getRefreshToken = () => {
-    if (typeof window !== "undefined") {
-      let token = localStorage.getItem("refresh-token");
+    if (typeof window !== 'undefined') {
+      let token = localStorage.getItem('refresh-token');
       return token;
     }
   };
 
-  console.log("accessToken", getToken())
+  console.log('accessToken', getToken());
   const refreshToken = async () => {
-    const res = await axios.post('https://api.maaambeyeducation.in/api/user/refresh-token', {}, {
-      headers: {
-        authorization: 'Bearer ' + getRefreshToken()
+    const res = await axios.post(
+      'https://api.maaambeyeducation.in/api/user/refresh-token',
+      {},
+      {
+        headers: {
+          authorization: 'Bearer ' + getRefreshToken(),
+        },
       }
-    })
-    console.log(res.data)
-    localStorage.setItem("access-token", res.data.data.access_token)
-  }
+    );
+    console.log(res.data);
+    localStorage.setItem('access-token', res.data.data.access_token);
+  };
 
-  console.log("date", Date.now() / 1000)
+  console.log('date', Date.now() / 1000);
   useEffect(() => {
-    console.log("date", Date.now() / 1000)
+    console.log('date', Date.now() / 1000);
     if (getToken()) {
-      if (jwtDecode(getToken()?.toString()).exp < Date.now() / 1000 && router.pathname.includes('admin')) {
-        refreshToken()
+      if (
+        jwtDecode(getToken()?.toString()).exp < Date.now() / 1000 &&
+        router.pathname.includes('admin')
+      ) {
+        refreshToken();
       }
     }
-  }, [getToken()])
+  }, [getToken()]);
 
   // useEffect(() => {
   //   if (typeof window !== "undefined") {
@@ -66,13 +73,13 @@ function MyApp({ Component, pageProps }) {
   //   }
   // }, []);
 
-  console.log("refreshtoken", getRefreshToken())
+  console.log('refreshtoken', getRefreshToken());
   let showLayout = true;
-  if (router.pathname.includes("admin") || router.pathname.includes("user")) {
+  if (router.pathname.includes('admin') || router.pathname.includes('user')) {
     showLayout = false;
   }
-  const login = router.pathname == "/user/login" ? true : false;
-  const dashboard = router.pathname.includes("admin") ? true : false;
+  const login = router.pathname == '/user/login' ? true : false;
+  const dashboard = router.pathname.includes('admin') ? true : false;
 
   return (
     <AuthProvider>
@@ -90,7 +97,6 @@ function MyApp({ Component, pageProps }) {
       )}
 
       {dashboard && (
-
         <CookiesProvider>
           <NextNProgress />
           {/* <AdminLayout> */}
@@ -100,7 +106,6 @@ function MyApp({ Component, pageProps }) {
           </div>
           {/* </AdminLayout> */}
         </CookiesProvider>
-
       )}
     </AuthProvider>
   );
