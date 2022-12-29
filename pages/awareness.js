459e8e3bf5827af2eb5232454030b20221abdbd6
pkaran12 <kp9594465364@gmail.com>
2022-12-29 17:41:11 +0530

    responsive

--- pages/awareness.js
+++ pages/awareness.js
@@ -1,280 +1,45 @@
+import { AwarenessData } from "../data/awarenessData";
+
 const Awareness = () => {
   return (
-    <>
-      <div className="h-full w-full flex justify-center items-center flex-col px-10 py-5">
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#e9edef] mx-5 ">
-            <span className="text-3xl font-semibold my-2">Nutrition</span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/lifestyle-01.svg"
-                className="w-[100%] h-[70vh]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              Is your body getting nutrients needed? what are sources for
-              vitamins and how much you need in your diet? Taking too many
-              vitamins can be unhealthy and even dangerous. Find out how to be
-              sure youre getting the right amount.
-            </p>
-            <p className="px-2 mb-1 ">
-              Vitamin D is essential for healthy bones and a lack of it can lead
-              to health problems including cardiovascular disease. Calcium and
-              vitamin D are important to prevent bone loss and osteoporosis.
-              Learn where to get the calcium and vitamin D your body needs.
-            </p>
-            <p className="px-2 ">
-              Vitamin B12 is essential for good health but some people may not
-              be getting enough. Learn about symptoms of a vitamin B12
-              deficiency and how it can be treated. Find out how much fiber you
-              need and where to get it. How to get omega-3 fatty acids in your
-              diet?
-              <button className="bg-red-400 mt-6 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#aadeda] mx-5 ">
-            <span className="text-3xl font-semibold py-2">Lifestyle</span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/Nutrition 2-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              A lot of times people think healthy lifestyles are about food
-              only. Its not just food its a multiplicity of things. Which
-              includes following.
-            </p>
-            <p className="px-2 mb-1 ">
-              AIR - Breathing in deeply through the nose into the stomach (belly
-              button) and out through the mouth really creates a healthy way of
-              breathing. It will oxygenate your body make your body vital and
-              alkalize your body as well. Water - Hydration; drink water every
-              day when you first wake up. This will hydrate your digestive
-              system and your body. Sunshine - Get out into the sun for 20-30
-              minutes.{" "}
-            </p>
-            <p className="px-2  ">
-              Exercise- Get the body moving; life is movement. Find a sport or
-              exercise. Bare minimum you should be walking at least 30-45
-              minutes almost every day. Whole food is complete and has
-              everything your body needs. Eat as fresh as you can as much as you
-              can! Eat at least 60% fresh fruits salads and vegetables.
-              Relationship-Focus on your relationships and do what you can to
-              improve them. Sleep - Get some good quality sleep – at least eight
-              hours a night to recharge your batteries.
-              <button className="bg-red-400 mt-6 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#baedf4] mx-5 ">
-            <span className="text-3xl font-semibold">Yoga & Exercise</span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/yoga & exercise-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              Yoga and exercise help in improving physical and mental health for
-              people of all ages. Yoga can help in recovering patients and for
-              patients with chronic condition it can become part of treatment
-              potentially help in healing. Slow movements and deep breathing
-              increase blood flow and warm up muscles while holding a pose can
-              build strength. Yoga improves strength balance and flexibility. It
-              helps in muscle pains ease arthritis symptoms healthy heart and
-              relaxes for better sleep
-            </p>
-            <p className="px-2 ">
-              Regular exercise makes the heart stronger and the lungs fitter
-              enabling the cardiovascular system to deliver more oxygen to the
-              body with every heartbeat and the pulmonary system to increase the
-              maximum amount of oxygen that the lungs can take in. It helps in
-              lowering blood pressure and decreases LDL – bad cholesterol and
-              helps in increasing HDL – good cholesterol.
-              <button className="bg-red-400 mt-2 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#e2d7bf] mx-5 ">
-            <span className="text-3xl font-semibold">Meditation</span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/meditation-01-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              Most meditation practices were developed within a spiritual
-              context and held as their ultimate goal some type of spiritual
-              growth personal transformation or transcendental experience. As a
-              health care intervention however meditation may be effective
-              regardless of peoples cultural spiritual or religious background.
-            </p>
-            <p className="px-2">
-              Meditation has numerous health benefits other than keeping your
-              mind in a balanced state. It helps in all brain activities and all
-              brain problems. It helps in stress anxiety depression insomnia
-              pains chronic disorders like cancer sexual dysfunction etc.
-              Meditation has been proposed to have many beneficial effects on
-              heart and blood vessel cardiovascular health. With meditation
-              people regulate their focus on aspects of inner or outer
-              experience. Any activity that brings peace of mind and can be done
-              mindfully can be considered meditation.{" "}
-              <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#d3cab7] mx-5">
-            <span className="text-3xl font-semibold">Home Remedies</span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/home remedies-01-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              History of using plant resources for treating diseases can be
-              dated back to 6000 to 4000 BCE the Buddhist period. AYM has a vast
-              literature in Sanskrit and various Indian languages covering
-              various aspects of diseases therapeutics and pharmacy
-            </p>
-            <p className="px-2 ">
-              In olden days our ancestors were doing most of the treatments at
-              home by different natural ways of taking water/ heat treatment
-              etc. with other natural resources available at home with usual
-              herbs available in kitchen. Those things had o side effects
-              negligible costs and were effective. In recent years increasing
-              numbers of people have been choosing herbal medicines or products
-              to improve their health conditions either alone or in combination
-              with others. Herbs are staging a comeback and herbal “renaissance”
-              occurs all over the world. According to the World Health
-              Organization 75% of the worlds populations are using herbs for
-              basic healthcare needs.
-              <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#e2d7bf] mx-5">
-            <span className="text-3xl font-semibold">
-              Naturopathy/ Acupressure/ Acupuncture
-            </span>
-            <div className="flex justify-center items-center flex-col my-3">
-              <img
-                src="images/awareness/Acupuncture-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2">
-              Basic principals of nature cure are – body heals by itself. Main
-              cause of disease is enervation. Deposit of metabolic end-products
-              in the body is disease. Acute disease is a remedial process –
-              itself a cure. Food is building material does not increase
-              vitality. Fasting provides an opportunity to the body to heal
-              itself. Exercise or physical activity keeps the balance between
-              nutrition and drainage. External treatments whether natural
-              allopathic ayurvedic or homeopathy – give only relief do not cure.
-              Patients own will to get well determination and faith are
-              necessary things for nature cure.
-              <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-5 bg-[#d3ebf3] mx-5">
-            <span className="text-3xl font-semibold">Ayurveda</span>
-            <div className="flex justify-center items-center my-3 flex-col">
-              <img
-                src="images/awareness/ayurveda-01-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              Ayurveda believes that the entire universe is composed of five
-              elements: Vayu (Air) Jala (Water) Aakash (Space or ether) Prithvi
-              (Earth) and Teja (Fire). These five elements (referred to as
-              Pancha Mahabhoota in Ayurveda) are believed to form the three
-              basic humors of human body in varying combinations. They developed
-              Ayurveda as a science to balance these elements in our bodies
-            </p>
-            <p className="px-2 ">
-              The second key principle of Ayurveda and the aspect that sets it
-              apart from most medical practices is that Ayurvedas main purpose
-              is the maintenance of good health. Ayurvedic medicine does offer
-              treatments for health problems but treating disease is a secondary
-              function. The primary intention of Ayurvedic practice has always
-              been to prevent disease or health problems arising in the first
-              place
-              <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
-                Know More
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="py-1 text-base">
-          <div className="px-4 py-3 bg-[#d3ebf3] mx-5">
-            <span className="text-3xl font-semibold">Homeopathy</span>
-            <div className="flex justify-center items-center my-3 flex-col">
-              <img
-                src="images/awareness/homeopathy-01.svg"
-                className="w-[75%] h-[75%]"
-              />
-            </div>
-            <p className="px-2 mb-1">
-              The homoeopathic system of medicine is one of the finest systems
-              of cure available to mankind. It achieves in totality the ideal
-              laid down by its originator Dr. Samuel Hahnemann. The highest
-              ideal of cure is rapid gentle and permanent restoration of the
-              health or removal and annihilation of the disease in its whole
-              extent in the shortest most reliable and most harmless way on
-              easily comprehensible principles
-            </p>
-            <p className="px-2">
-              The harmlessness of the homoeopathic system cannot be
-              overemphasized.
-              <div className="flex justify-between">
-                <button className="bg-orange-500 mt-4 text-white px-3 py-1 text-base rounded-md hover:border-blue-600 border-solid">
-                  Diagnose Disease
-                </button>
-                <button className="bg-red-400  mt-4 text-xs text-white font-sans px-2 py-2 lg:px-5 lg:py-2 lg:text-xl">
+    <div className="flex justify-center">
+      <div className="w-[90%] xl:w-[80%]  mt-10">
+        <div className="flex flex-col gap-3">
+          {AwarenessData.map((data) => {
+            return (
+              <div className="bg-[#e9edef] p-4">
+                <span className="text-3xl  font-semibold my-2">
+                  {data.title}
+                </span>
+                <div className="flex justify-center items-center flex-col md:my-3 my-0">
+                  <div>
+                    <img
+                      src={data.img}
+                      className=" md:w-[100%] md:h-[70vh] h-[30vh]"
+                    />
+                  </div>
+                </div>
+                <div className="md:h-full h-[40vh]  overflow-auto">
+                  {data.para.map((p) => {
+                    return <p className=" px-2 mb-1">{p}</p>;
+                  })}
+                </div>
+                <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:text-xl">
                   Know More
                 </button>
+                <div>
+                  {`Homeopathy` == data.title ? (
+                    <button className="bg-orange-500 mt-4 text-white px-3 py-1 text-base rounded-md hover:border-blue-600 border-solid">
+                      Diagnose Disease
+                    </button>
+                  ) : null}
+                </div>
               </div>
-            </p>
-          </div>
+            );
+          })}
         </div>
-        {/* <div className="border-black border-solid border-2 py-3">
-                <div className="px-2">
-                <span className="text-3xl"></span>
-
-                </div>
-            </div> */}
-        {/* <div className="border-black border-solid border-2 py-3">
-                <div className="px-2">
-                <span className="text-3xl"></span>
-
-                </div>
-            </div> */}
       </div>
-    </>
+    </div>
   );
 };
 
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import Banner from '../components/multiusable/banner';
-import Cards from './Application/cards';
+import React from "react";
+import Banner from "../components/multiusable/banner";
+import Cards from "./Application/cards";
 const blogs = () => {
   const array = [
     {
-      image: '/images/image.jpeg',
-      title: 'First Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "First Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'Second Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "Second Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'Third Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "Third Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'Fourth Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "Fourth Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'Fifth Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "Fifth Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'Sixth Blog',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "Sixth Blog",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'abc',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "abc",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
     {
-      image: '/images/image.jpeg',
-      title: 'abc',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever',
+      image: "/images/image.jpeg",
+      title: "abc",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever",
     },
   ];
   return (
     <div className="h-max">
-      <div className="md:flex grid md:justify-between grid-col-2 ">
+      <div className="md:flex grid md:justify-between grid-col-2  ">
         <div className="flex flex-1 justify-start md:text-start text-center items-center ">
           <p className=" font-semibold font-sans text-8xl text-sky-800 border-l-4 border-sky-800 pl-7">
             READ OUR <br /> BLOG
           </p>
         </div>
-        <div className="md:w-1/2 md:h-1/2">
+        <div className="md:w-1/2 w-full md:h-1/2 h-full">
           <Banner image="/images/home-images/blog-1.png" />
         </div>
       </div>
