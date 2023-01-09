9185bebffb7af7a8d3e204d7d0f87d09fdeb5da5
Ninad Muranjan <hrxprog69@gmail.com>
2023-01-09 11:58:50 +0530

    added dato cms

--- pages/about-us.js
+++ pages/about-us.js
@@ -36,32 +36,29 @@ export default function About({ data }) {
           src={data.aboutpage.mainImage.url}
         />
         <div className="flex flex-col items-center w-full xl:w-full ">
-          <div
-            id="about-main"
-            // style={{ background: `${data.about.addBackground.hex}` }}
-            className="flex flex-col lg:grid grid-cols-[50%_50%] h-full xl:h-[calc(100vh-80px)] px-2 text-blackp"
-          >
-            <div className="text-sm px-4 md:py-5">
-              {data.aboutpage.aboutSection.map((ind) => {
-                return (
-                  <p className="font-semibold text-base text-gray-600 mb-3 mt-3">
-                    {ind.content}
-                  </p>
-                );
-              })}
-              <div className="h-96 my-2 md:h-[60vh] overflow-y-auto">
-                <div className="text-base my-0 text-black">
-                  {/* {data.about.addContent} */}
+          {data.aboutpage.aboutSection.map((ind) => {
+            return (
+              <div
+                id="about-main"
+                style={{ background: `${ind.background.hex}` }}
+                className="flex flex-col lg:grid grid-cols-[50%_50%] h-full xl:h-[calc(100vh-80px)] px-2 text-blackp"
+              >
+                <div className="text-sm px-4 md:py-5">
+                  <div className="h-96 my-2 md:h-[60vh] overflow-y-auto">
+                    <h3>{ind.title}</h3>
+                    <div className="text-base my-0 text-black">
+                      <p className="font-semibold text-base text-gray-600 mb-3 mt-3">
+                        {ind.content}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div id="abou" className="px-2 flex ">
+                  <img src={ind.image.url} />
                 </div>
               </div>
-            </div>
-            <div id="abou" className="px-2 flex ">
-              <img
-                // src={data.about.addImage[0].url}
-                className="h-72 lg:h-full w-full md:my-0 flex justify-center items-center border-none"
-              />
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- pages/awareness.js
+++ pages/awareness.js
@@ -1,68 +1,74 @@
-// import { AwarenessData } from '../data/awarenessData';
 import { request } from '../utils/request';
 
 const AWARENESS_QUERY = `query MyQuery {
-  awareness {
-    content
-    title
-    background {
-      hex
-    }
-    image {
-      url
+  awarenesspage {
+    awarenessSection {
+      background{
+        hex
+      }
+      title
+      image {
+        url
+      }
+      content
     }
   }
 }
 `;
 
 export async function getStaticProps() {
-  const data1 = await request({
+  const dataa = await request({
     query: AWARENESS_QUERY,
     variables: { limit: 10 },
   });
   return {
-    props: { data1 },
+    props: { dataa },
   };
 }
 
-export default function Awareness({ data1 }) {
-  console.log(data1, 'bdjewjdjnewsydebbydw');
+export default function Awareness({ dataa }) {
+  console.log(dataa, 'bdjewjdjnewsydebbydw');
   return (
-    <div className="flex justify-center">
-      <div className="w-[90%] xl:w-[90%] mt-10">
-        <div className="flex flex-col">
-          <div
-            style={{ background: `${data1.awareness.background.hex}` }}
-            className="p-4 my-6"
-          >
-            <span className="text-3xl  font-semibold my-2">
-              {data1.awareness.title}
-            </span>
-            <div className="flex justify-center items-center flex-col md:my-3 my-0">
-              <div>
-                <img
-                  src={data1.awareness.image[0].url}
-                  className=" md:w-[100%] md:h-[70vh] h-[30vh]"
-                />
+    <div>
+      {dataa.awarenesspage.awarenessSection.map((con) => {
+        return (
+          <div className="flex justify-center items-center flex-col">
+            <div className="w-[90%] xl:w-[90%] mt-10">
+              <div className="flex flex-col">
+                <div
+                  style={{ background: `${con.background.hex}` }}
+                  className="p-4 my-6"
+                >
+                  <span className="text-3xl  font-semibold my-2">
+                    {con.title}
+                  </span>
+                  <div className="flex justify-center items-center flex-col md:my-3 my-0">
+                    <div>
+                      <img
+                        src={con.image.url}
+                        className=" md:w-[100%] md:h-[70vh] h-[30vh]"
+                      />
+                    </div>
+                  </div>
+                  <div className="md:h-full h-[40vh] overflow-auto">
+                    <p className=" px-2 mb-1">{con.content}</p>
+                  </div>
+                  <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:text-xl">
+                    Know More
+                  </button>
+                  <div>
+                    {`Homeopathy` === con.title ? (
+                      <button className="bg-orange-500 mt-4 text-white px-3 py-1 text-base rounded-md hover:border-blue-600 border-solid">
+                        Diagnose Disease
+                      </button>
+                    ) : null}
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="md:h-full h-[40vh] overflow-auto">
-              <p className=" px-2 mb-1">{data1.awareness.content}</p>
-            </div>
-            <button className="bg-red-400 mt-4 float-right text-xs text-white font-sans px-2 py-2 lg:text-xl">
-              Know More
-            </button>
-            <div>
-              {/* {`Homeopathy` == data.title ? ( */}
-              <button className="bg-orange-500 mt-4 text-white px-3 py-1 text-base rounded-md hover:border-blue-600 border-solid">
-                Diagnose Disease
-              </button>
-              {/* ) : null} */}
-            </div>
           </div>
-          );
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
