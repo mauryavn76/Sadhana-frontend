6a8e159d483616e604f94987288599048eabc7f8
hrx21 <hrxprog69@gmail.com>
2023-01-18 17:16:29 +0530

    build

--- pages/diagnose-disease.js
+++ pages/diagnose-disease.js
@@ -1,8 +1,8 @@
 const DiagnoseDisease = () => {
     return ( 
         <>
-        <div className="h-full w-full flex justify-center items-center md:px-20 px-3 py-20">
-            <img className="border-white border-solid" src="images/dd.png" />
+        <div className="h-full w-full flex justify-center items-center md:py-24 py-3 px-3 md:px-16">
+            <img className="border-white border-solid" src="images/home-diagnose.jpg" />
         </div>
         </>
      );
