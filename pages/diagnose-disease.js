e136d0896f4ea3fe7b087505c71c6889c24231cc
pkaran12 <kp9594465364@gmail.com>
2023-02-15 17:38:27 +0530

    fixed about us

--- pages/diagnose-disease.js
+++ pages/diagnose-disease.js
@@ -1,7 +1,7 @@
 const DiagnoseDisease = () => {
     return ( 
         <>
-        <div className="h-full w-full flex justify-center items-center px-14 py-14">
+        <div className="h-full w-full flex justify-center items-center p-4">
             <img className="border-white border-solid" src="images/home-diagnose.jpg" />
         </div>
         </>
